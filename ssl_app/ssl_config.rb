637190aa1861ef0946fa6fa02fdcb9e1a1bbec3b
Patrik Ragnarsson <patrik@starkast.net>
2020-04-10 10:11:44 +0200

    listen to http too, and log, and comment for later

--- ssl_app/ssl_config.rb
+++ ssl_app/ssl_config.rb
@@ -2,10 +2,14 @@ key  = File.expand_path("./puma_key.pem")
 cert = File.expand_path("./puma_cert.pem")
 ca   = File.expand_path("~/Library/Application Support/mkcert/rootCA.pem")
 
+bind "tcp://127.0.0.1:9291"
+
 ssl_bind "127.0.0.1", 9292, cert: cert,
                             key:  key,
                             ca:   ca,
                             verify_mode: "peer"
+
+log_requests true
 
 app do |env|
   [200, {}, ["embedded app"]]
