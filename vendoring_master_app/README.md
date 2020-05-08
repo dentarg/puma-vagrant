14db8f3fb0be6d94c2d66825125fc2220757a27f
Patrik Ragnarsson <patrik@starkast.net>
2020-05-08 13:46:30 +0200

    use puma master, vendored

--- vendoring_master_app/README.md
+++ vendoring_master_app/README.md
@@ -5,3 +5,18 @@ Setup:
 ```
 bundle pack --all
 ```
+
+Switch to Puma from GitHub master
+
+```
+bundle
+```
+
+Boom
+
+```
+$ bundle exec puma -C config.rb
+bundler: failed to load command: puma (/Users/dentarg/.gem/ruby/2.6.6/bin/puma)
+LoadError: cannot load such file -- puma/puma_http11
+...
+```
---
+++ vendoring_master_app/vendor/cache/puma-6234e4b057ff/lib/puma/detect.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Puma
+  IS_JRUBY = defined?(JRUBY_VERSION)
+
+  def self.jruby?
+    IS_JRUBY
+  end
+
+  IS_WINDOWS = RUBY_PLATFORM =~ /mswin|ming|cygwin/
+
+  def self.windows?
+    IS_WINDOWS
+  end
+
+  def self.mri?
+    RUBY_ENGINE == 'ruby' || RUBY_ENGINE.nil?
+  end
+
+  def self.forkable?
+    ::Process.respond_to?(:fork)
+  end
+end
